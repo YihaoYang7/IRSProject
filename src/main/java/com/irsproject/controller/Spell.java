d92a62eefe1d3ed6a34d27331e91eb6523da6e1a
s-1-0 <91892355+s-1-0@users.noreply.github.com>
2021-11-16 20:16:04 +0800

    Add files via upload

---
+++ src/main/java/com/irsproject/controller/Spell.java
@@ -0,0 +1,66 @@
+package com.irsproject.controller;
+
+import com.google.gson.Gson;
+import org.apache.lucene.search.spell.JaroWinklerDistance;
+import org.apache.lucene.search.spell.SpellChecker;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.*;
+
+
+
+@Controller
+public class Spell {
+
+    @RequestMapping("/spell")
+    @ResponseBody
+    public String SpellChecker(@RequestBody Map<String, String> p)  {
+        String spellResults = "";
+        try
+        {
+            LinkedList<String> spellList = new LinkedList<String>();
+            Gson gson = new Gson();
+            String keywords = p.get("keywords");
+            ArrayList<String> list = new ArrayList<String>();
+            StringTokenizer st = new StringTokenizer(keywords, " ");
+            while (st.hasMoreTokens())
+                list.add(st.nextToken());
+            String[] keywordsarr = new String[list.size()];
+            String[] suggestions = new String[list.size()];
+            int count = 0;
+            for(int i = 0 ;i < list.size() ; i ++) {
+                keywordsarr[i] = list.get(i);
+                Directory directory = FSDirectory.open(Paths.get("spell_index"));
+                SpellChecker checker = new SpellChecker(directory);
+                checker.setStringDistance(new JaroWinklerDistance());
+                String[] str = checker.suggestSimilar(keywordsarr[i], 5);
+                if (str.length > 0)
+                    suggestions[i] = str[0];
+                else
+                    suggestions[i] = keywordsarr[i] + " ";
+                if (keywordsarr[i].equals(suggestions[i].substring(0, suggestions[i].length() - 1)))
+                    count ++;
+                else
+                    keywordsarr[i] = suggestions[i];
+            }
+            if (count == list.size())
+                spellResults = gson.toJson(spellList);
+            else {
+                for (int i = 0; i < list.size(); i++) {
+                    spellResults = spellResults + " " + keywordsarr[i];
+                }
+                spellList.add(spellResults);
+                spellResults = gson.toJson(spellList);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return spellResults;
+    }
+}
