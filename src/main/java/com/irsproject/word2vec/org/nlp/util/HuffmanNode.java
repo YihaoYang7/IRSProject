6cfe92bc90c8fc63456f38b3e69e26a19364977b
s-1-0 <91892355+s-1-0@users.noreply.github.com>
2021-11-16 20:10:32 +0800

    Add files via upload

---
+++ src/main/java/com/irsproject/word2vec/org/nlp/util/HuffmanNode.java
@@ -0,0 +1,17 @@
+package com.irsproject.word2vec.org.nlp.util;
+
+
+public interface HuffmanNode extends Comparable<HuffmanNode> {
+
+    public void setCode(int c);
+
+    public void setFrequency(int freq);
+
+    public int getFrequency();
+
+    public void setParent(HuffmanNode parent);
+
+    public HuffmanNode getParent();
+
+    public HuffmanNode merge(HuffmanNode sibling);
+}
---
+++ src/main/java/com/irsproject/word2vec/word2vecMain/word2vecMain.java
@@ -0,0 +1,47 @@
+package com.irsproject.word2vec.word2vecMain;
+
+
+import com.irsproject.word2vec.org.nlp.vec.VectorModel;
+
+import java.util.Collections;
+import java.util.Set;
+
+
+import com.google.gson.Gson;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.*;
+
+
+@Controller
+public class word2vecMain {
+    @RequestMapping("/word2vec")
+    @ResponseBody
+    public String Word2Vec(@RequestBody Map<String, String> p) {
+        String wordResults = "";
+        String keywords = p.get("keywords");
+        Gson gson = new Gson();
+        String modelFilePath = "index.nn";
+        LinkedList wordList = new LinkedList();
+        ArrayList<String> arr = new ArrayList<String>();
+        StringTokenizer st = new StringTokenizer(keywords, " ");
+        while (st.hasMoreTokens())
+            arr.add(st.nextToken());
+        for (int i = 0; i < arr.size(); i++) {
+            VectorModel vm = VectorModel.loadFromFile(modelFilePath);
+            Set<VectorModel.WordScore> result1 = Collections.emptySet();
+            result1 = vm.similar(arr.get(i));
+            int count = 0;
+            for (VectorModel.WordScore we : result1) {
+                wordList.add(we.name);
+                count++;
+                if (count == 2)
+                    break;
+            }
+        }
+        wordResults = gson.toJson(wordList);
+        return wordResults;
+    }
+}
